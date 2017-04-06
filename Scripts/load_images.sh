3c383f1f18106bb8cc99bb23b9bbb763821d0177
Maxime Clerix <solidsnake444@hotmail.fr>
2017-04-06 11:44:17 +0200

    /

--- Scripts/load_images.sh
+++ Scripts/load_images.sh
@@ -1,5 +1,5 @@
 docker pull docker.io/clrxm/adop-nginx:0.1.0 &
-docker pull docker.io/clrxm/adop-gerrit:0.1.2 &
+docker pull docker.io/accenture/adop-gerrit:0.1.3 &
 docker pull registry.access.redhat.com/rhscl/mysql-56-rhel7 &
 docker pull docker.io/accenture/adop-nexus:0.1.3 &
 docker pull docker.io/selenium/node-firefox:2.53.0 &
@@ -7,7 +7,7 @@ docker pull docker.io/selenium/node-chrome:2.53.0 &
 docker pull docker.io/selenium/hub:2.53.0 &
 docker pull docker.io/accenture/adop-ldap:0.1.3 &
 docker pull docker.io/accenture/adop-sonar:0.2.0 &
-docker pull docker.io/accenture/adop-jenkins:0.1.5 &
+docker pull docker.io/accenture/adop-jenkins:0.2.6 &
 docker pull docker.io/accenture/adop-ldap-phpadmin:0.1.0 &
 docker pull docker.io/accenture/adop-ldap-ltb:0.1.0 &
 docker pull docker.io/accenture/adop-jenkins-slave:0.1.4 &
