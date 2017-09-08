c94ff9e90173f4c08b0dbeb5993933f8b2a5460f
Maxime Clerix <solidsnake444@hotmail.fr>
2017-09-08 13:55:59 +0200

    clean repository

--- askme_warmup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Run the script once for intializing your newly Openshift cluster with required image for AskMe
-oc new-build https://github.com/clerixmaxime/sti-grunt-nginx.git --context-dir=/1.0 --strategy=docker --to=sti-grunt-nginx -n openshift
